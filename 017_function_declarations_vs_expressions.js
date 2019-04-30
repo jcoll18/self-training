1ff86ea32e6f40994f5ebc9418815c8142cf7c14
Jessica Rowell <jessica.collins333@gmail.com>
2019-04-29 22:16:14 -0500

    lecture 23

---
+++ 017_function_declarations_vs_expressions.js
@@ -0,0 +1,16 @@
+console.log(fullName('Jessica', 'Rowell'));
+// console.log(otherFullName('Jessica', 'Rowell'));
+// console.log(thirdFullName('Jessica', 'Rowell'));
+// Function declaration
+function fullName(first, last) {
+    return first + " " + last;
+}
+// Function expression
+var otherFullName;
+otherFullName = function (first, last) {
+    return first + " " + last;
+};
+var thirdFullName = function (first, last) {
+    return first + " " + last;
+};
+//# sourceMappingURL=017_function_declarations_vs_expressions.js.map
---
+++ 017_function_declarations_vs_expressions.ts
@@ -0,0 +1,20 @@
+console.log(fullName('Jessica', 'Rowell'));
+// console.log(otherFullName('Jessica', 'Rowell'));
+// console.log(thirdFullName('Jessica', 'Rowell'));
+
+// Function declaration
+
+function fullName(first : string, last : string) : string {
+    return first + " " + last;
+}
+
+// Function expression
+var otherFullName : (first : string, last : string) => string;
+
+otherFullName = function (first : string, last : string) {
+    return first + " " + last;
+}
+
+var thirdFullName : (first : string, last : string) => string = function (first : string, last : string) {
+    return first + " " + last;
+}
