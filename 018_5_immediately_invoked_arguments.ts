816053602a36b82bbfb40b047e94713f1a013f9b
Jessica Rowell <jessica.collins333@gmail.com>
2019-05-01 21:41:53 -0500

    lecture 25

---
+++ 018_5_immediately_invoked_arguments.ts
@@ -0,0 +1,19 @@
+// Function expression
+var fullName : (first : string, last : string) => string;
+
+fullName = function(first : string, last : string) {
+    return first + " " + last;
+}
+
+console.log(fullName('Jessica', 'Rowell'));
+
+// Immediately invoked version
+(function(first : string, last : string) {
+    console.log(first + " " + last);
+})('Joshua', 'Rowell');
+
+// (function() {
+//     for (let name in names) {
+//         counter++;
+//     }
+// })();
